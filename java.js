923242dff2ddbf1c4ec0ba872a9b2f89f85a2e0a
keithlyang <keithluckiyang@gmail.com>
2021-12-21 11:09:02 -0600

    initial commit

---
+++ java.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        q: "Which of these is NOT like the others?",
+        options: ["Apples", "Bananas", "Oranges", "Potatoes"],
+        correct: "Potatoes"
+    },
+    {
+        q: "Which of these is NOT like the others?",
+        options: ["Spoon", "Fork", "Knife", "Car"],
+        correct: "Car"
+    },
+    {
+        q: "Which of these is NOT like the others?",
+        options: ["Volleyball", "Soccer", "Swimming", "Drinking Coffee"],
+        correct: "Drinking Coffee"
+    },
+    {
+        q: "Which of these is NOT like the others?",
+        options: ["Red", "Blue", "Yellow", "Dragons"],
+        correct: "Dragons"
+    },   
+]
+
+var time;
+var secondLeft = 60;
+var timerCount;
